a4fa1e3472f54d0c61089bbe949d4ab48f1fbc95
CaioCesarP <caiocesarworks@gmail.com>
2022-06-04 17:04:11 -0300

    Adicionando estilização, preparando para jogo da velha e melhorando codigo antigo

--- script.js
+++ script.js
@@ -1,38 +1,79 @@
-let data = [
-  { nome: "Antonio Francisco", idade: 26, aprovado: false, categoria: "A" },
-  { nome: "João Alberto", idade: 24, aprovado: true, categoria: "C" },
-  { nome: "Vitor Emanuel", idade: 19, aprovado: true, categoria: "A" },
-  { nome: "Gustavo Dantas", idade: 20, aprovado: true, categoria: "C" },
-  { nome: "Italo Aguiar", idade: 18, aprovado: false, categoria: "B" },
-];
+// let data = [
+//   { nome: "Antonio Francisco", nascimento: "2000-03-26", aprovado: true, categoria: "A" },
+//   { nome: "João Alberto", nascimento: "1999-06-19", aprovado: true, categoria: "C" },
+//   { nome: "Vitor Emanuel", nascimento: "2022-04-06", aprovado: false, categoria: "A" },
+//   { nome: "Gustavo Dantas", nascimento: "2001-11-08", aprovado: true, categoria: "C" },
+//   { nome: "Italo Aguiar", nascimento: "2000-10-30", aprovado: false, categoria: "B" },
+// ];
 
-function exibirAprovados(data, categoria) {
-  let nomes = [];
-  data.forEach((obj) => {
-    obj.categoria === categoria &&
-      obj.aprovado &&
-      obj.idade >= 18 &&
-      nomes.push(obj.nome);
-  });
-  console.log(
-    `%c-- Usuários aprovados na categoria ${categoria} --`,
-    `color: rgba(155, 200, 255);`
-  );
-  nomes.length > 0
-    ? console.log(
-        `%c${nomes.join(", ")}\n---------`,
-        `color: rgba(00, 255, 00);`
-      )
-    : console.log(
-        "%cNenhum usuário com está categoria aprovado.\n---------",
-        `color: rgba(255, 00, 00);`
-      );
+// const todayData = () => {
+//   var get = new Date();
+//   var date = get.getFullYear()+"-"+(get.getMonth()+1)+"-"+get.getDate();
+//   return (new Date(date));
+// }
+
+// const exibirAprovados = (data, categoria) => {
+//   let nomes = [];
+//   console.log(todayData() - new Date(data[2].nascimento))
+//   console.log(todayData())
+//   console.log(new Date(data[2].nascimento));
+//   data.forEach((obj) => {
+//     let date = new Date(obj.nascimento);
+//     obj.categoria == categoria &&
+//       obj.aprovado &&
+//         nomes.push(obj.nome);
+//   });
+//   console.log(
+//     `%c-- Usuários aprovados na categoria ${categoria} --`,
+//     `color: rgba(155, 200, 255);`
+//   );
+//   nomes.length > 0
+//     ? console.log(
+//         `%c${nomes.join(", ")}\n---------`,
+//         `color: rgba(00, 255, 00);`
+//       )
+//     : console.log(
+//         "%cNenhum usuário com está categoria aprovado.\n---------",
+//         `color: rgba(255, 00, 00);`
+//       );
+// }
+
+// exibirAprovados(data, "A");
+
+// exibirAprovados(data, "B");
+
+// exibirAprovados(data, "C");
+
+// exibirAprovados(data, "D");
+
+
+
+
+
+
+const box = document.querySelectorAll(".box");
+
+
+
+const changeValuesInterface = () => {
+  console.clear();
+  let vez = "X";
+  
+  for(let i = 0; i < 10; i++){
+    box[i].addEventListener("click", () => {
+      box[i].innerHTML === undefined &&
+        vez === "X" 
+        ? (
+          box[i].innerHTML = "X" &&
+          (vez = "O")
+        ) 
+        : (
+          box[i].innerHTML = "O" &&
+          (vez = "X")
+        )
+      console.log(box[i])
+    });
+  }
 }
 
-exibirAprovados(data, "A");
-
-exibirAprovados(data, "B");
-
-exibirAprovados(data, "C");
-
-exibirAprovados(data, "D");
+changeValuesInterface();
